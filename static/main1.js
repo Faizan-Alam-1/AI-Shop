7eb7636a228ed99f42b14a59c6ed07c15a3e3827
Faizan Alam <faizanalam2030@gmail.com>
2022-07-07 22:48:05 +0530

    final

---
+++ static/main1.js
@@ -0,0 +1,45 @@
+import { loadGLTF } from "/libs/loader.js";
+
+const THREE = window.MINDAR.FACE.THREE;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const start = async () => {
+   
+
+    const mindarThree = new window.MINDAR.FACE.MindARThree({
+      // container: document.body,
+      container: document.querySelector("#My-ar"),
+    });
+    const { renderer, scene, camera } = mindarThree;
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    const occluder = await loadGLTF("/sparkar-occluder/headOccluder.glb");
+    const occluderMaterial = new THREE.MeshBasicMaterial({ colorWrite: false });
+    occluder.scene.scale.set(0.1, 0.1, 0.1);
+    occluder.scene.position.set(0, -0.3, 0.15);
+    occluder.scene.traverse((o) => {
+      if (o.isMesh) {
+        o.material = occluderMaterial;
+      }
+    });
+    occluder.scene.renderOrder = 0;
+
+    const occluderAnchor = mindarThree.addAnchor(5);  
+    occluderAnchor.group.add(occluder.scene);
+
+    const glasses = await loadGLTF("../Product_model/black_metal/scene.gltf");
+    glasses.scene.scale.set(0.07, 0.07, 0.07);
+    glasses.scene.renderOrder = 1;
+
+    const anchor = mindarThree.addAnchor(5);
+    anchor.group.add(glasses.scene);
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  };
+  start();
+});
